66223a25fe0bace612073bd2bc683283f9194dfb
Marks Gusakovs <marks.olivers1@gmail.com>
2022-02-09 18:23:14 +0100

    Update flag_p.c
    fd

--- src/flag_p.c
+++ src/flag_p.c
@@ -17,6 +17,6 @@ int	flag_p(va_list lst)
 	void	*n;
 
 	n = va_arg(lst, void *);
-	ft_putstr("0x");
+	ft_putstr_fd("0x", 1);
 	return (ft_putnbr_base((unsigned long long)n, "0123456789abcdef") + 2);
 }
